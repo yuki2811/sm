4681ff4eb56322f04f0f2ba6ad1946bfab126626
yuki2811 <yuki2811@users.noreply.github.com>
2020-10-22 09:15:58 +0700

    test2

--- resources/views/page/login.blade.php
+++ resources/views/page/login.blade.php
@@ -38,7 +38,7 @@
               <div class="col-lg-6">
                 <div class="p-5">
                   <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Chương trình quản lý điểm AAAAAAAAAA</h1>
+                    <h1 class="h4 text-gray-900 mb-4">Chương trình quản lý điểm BBBBBBBBBB</h1>
                   </div>
                    @if(count($errors)>0)
                       <div class="alert alert-danger">
